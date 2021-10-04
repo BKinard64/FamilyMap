351d30e9267c7b4cacc006ea0a6a5c60a0cc86fa
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-04 12:55:29 -0600

    Implement register and login methods in UserDao class

--- FamilyMapServerStudent-master/src/main/java/dao/UserDao.java
+++ FamilyMapServerStudent-master/src/main/java/dao/UserDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import model.AuthToken;
+import model.User;
+
+import java.sql.Connection;
+
 public class UserDao {
+    private final Connection conn;
+
+    public UserDao(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void register(User user) {}
+
+    public AuthToken login(User user) {return null;}
 }
