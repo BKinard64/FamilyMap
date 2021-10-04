bb1c58e2ad3d8b4d461af10bcf6044ebbe4413db
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-04 12:58:31 -0600

    Implement find method of PersonDao class

--- FamilyMapServerStudent-master/src/main/java/dao/PersonDao.java
+++ FamilyMapServerStudent-master/src/main/java/dao/PersonDao.java
@@ -1,4 +1,17 @@
 package dao;
 
+import model.Person;
+
+import java.sql.Connection;
+
 public class PersonDao {
+    private final Connection conn;
+
+    public PersonDao(Connection conn) {
+        this.conn = conn;
+    }
+
+    public Person find(String personID) {return null;}
+
+
 }
