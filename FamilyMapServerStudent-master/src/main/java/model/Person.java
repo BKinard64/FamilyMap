f1417c7cf62b6590e24795d9ea4266eed32fba99
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-04 06:47:05 -0600

    Create model.person class

---
+++ FamilyMapServerStudent-master/src/main/java/model/Person.java
@@ -0,0 +1,130 @@
+package model;
+
+import java.util.Objects;
+
+public class Person {
+    private String id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String fatherID;
+    private String motherID;
+    private String spouseID;
+
+    public Person() {
+        id = null;
+        username = null;
+        firstName = null;
+        lastName = null;
+        gender = null;
+        fatherID = null;
+        motherID = null;
+        spouseID = null;
+    }
+
+    public Person(String id, String username, String firstName, String lastName, String gender, String fatherID, String motherID, String spouseID) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.fatherID = fatherID;
+        this.motherID = motherID;
+        this.spouseID = spouseID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFatherID() {
+        return fatherID;
+    }
+
+    public void setFatherID(String fatherID) {
+        this.fatherID = fatherID;
+    }
+
+    public String getMotherID() {
+        return motherID;
+    }
+
+    public void setMotherID(String motherID) {
+        this.motherID = motherID;
+    }
+
+    public String getSpouseID() {
+        return spouseID;
+    }
+
+    public void setSpouseID(String spouseID) {
+        this.spouseID = spouseID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id.equals(person.id) && Objects.equals(username, person.username) && firstName.equals(person.firstName) &&
+                lastName.equals(person.lastName) && gender.equals(person.gender) &&
+                Objects.equals(fatherID, person.fatherID) && Objects.equals(motherID, person.motherID) &&
+                Objects.equals(spouseID, person.spouseID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, firstName, lastName, gender, fatherID, motherID, spouseID);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", fatherID='" + fatherID + '\'' +
+                ", motherID='" + motherID + '\'' +
+                ", spouseID='" + spouseID + '\'' +
+                '}';
+    }
+}
