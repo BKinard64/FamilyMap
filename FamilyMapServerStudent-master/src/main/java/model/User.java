f5d7e8e92861ceec1ef802dc451dcfba6a61e123
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-04 06:39:35 -0600

    Create model.user class

---
+++ FamilyMapServerStudent-master/src/main/java/model/User.java
@@ -0,0 +1,115 @@
+package model;
+
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String id;
+
+    public User() {
+        username = null;
+        password = null;
+        email = null;
+        firstName = null;
+        lastName = null;
+        gender = null;
+        id = null;
+    }
+
+    public User(String username, String password, String email, String firstName, String lastName, String gender, String id) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return username.equals(user.username) && password.equals(user.password) && email.equals(user.email) && firstName.equals(user.firstName) && lastName.equals(user.lastName) && gender.equals(user.gender) && id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, email, firstName, lastName, gender, id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
