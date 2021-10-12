64bc5748843078d9d8305528635b7afbe1762c0b
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-12 08:03:12 -0600

    Implement UserDao insert method

--- FamilyMapServerStudent-master/src/main/java/dao/UserDao.java
+++ FamilyMapServerStudent-master/src/main/java/dao/UserDao.java
@@ -3,6 +3,8 @@ package dao;
 import model.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * A data-access object for the User table.
@@ -27,7 +29,27 @@ public class UserDao {
      *
      * @param user the User to be inserted into the table.
      */
-    public void insert(User user) {}
+    public void insert(User user) throws DataAccessException {
+        // Create string to pass into the connection's prepareStatement method
+        String sql = "INSERT INTO user (username, pwd, email, first_name, last_name, gender, person_id) " +
+                "VALUES(?,?,?,?,?,?,?)";
+        // Create a PreparedStatement object using the string above
+        try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+            // Pass the members of the provided User object to the Statement object
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getEmail());
+            stmt.setString(4, user.getFirstName());
+            stmt.setString(5, user.getLastName());
+            stmt.setString(6, user.getGender());
+            stmt.setString(7, user.getPersonID());
+
+            // Execute the statement with the passed-in members
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while inserting into the database");
+        }
+    }
 
     /**
      * Find the User with the given username.
