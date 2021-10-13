33a1314becd300af42eda5be40d87d242d910550
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-12 18:46:04 -0600

    Pass insertFail test on UserDaoTest class

--- FamilyMapServerStudent-master/src/test/java/dao/UserDaoTest.java
+++ FamilyMapServerStudent-master/src/test/java/dao/UserDaoTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.crypto.Data;
 import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.*;
