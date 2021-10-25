c34c56ad6a4f4be8d85b06bb51a9bbfeb7b87aa5
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-25 17:36:52 -0600

    Edit Person Request Object

--- FamilyMapServerStudent-master/src/main/java/service/requests/PersonRequest.java
+++ FamilyMapServerStudent-master/src/main/java/service/requests/PersonRequest.java
@@ -10,15 +10,24 @@ public class PersonRequest {
     private String personID;
 
     /**
+     * The authToken sent with the request.
+     */
+    private String authToken;
+
+    /**
      * Create a PersonRequest Object.
      *
      * @param personID the ID of the person to be found.
+     * @param authToken the authToken sent with the request.
      */
-    public PersonRequest(String personID) {
+    public PersonRequest(String personID, String authToken) {
         this.personID = personID;
+        this.authToken = authToken;
     }
 
     public String getPersonID() {
         return personID;
     }
+
+    public String getAuthToken() { return authToken; }
 }
