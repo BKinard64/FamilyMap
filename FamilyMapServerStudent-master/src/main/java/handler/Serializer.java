82f5946324dcd56fdbba867e999702bc0aa05177
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-25 12:59:08 -0600

    Implement serialize method of Serializer Class

--- FamilyMapServerStudent-master/src/main/java/handler/Serializer.java
+++ FamilyMapServerStudent-master/src/main/java/handler/Serializer.java
@@ -1,15 +1,30 @@
 package handler;
 
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
 import service.results.Result;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class Serializer {
-    public void serialize(Result r) {
+    private HttpExchange exchange;
+    private Gson gson;
 
+    public Serializer(HttpExchange exchange) {
+        this.exchange = exchange;
+        gson = new Gson();
     }
 
-    public void deserialize(File file) {
+    public void serialize(Result result) throws IOException {
+        Writer resBody = new OutputStreamWriter(exchange.getResponseBody());
+        gson.toJson(result, resBody);
+        resBody.close();
+    }
+
+    public void deserialize() {
 
     }
 }
