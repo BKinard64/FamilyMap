a6f78156d23770ada46a5452f8128484020c988b
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-26 17:15:41 -0600

    Implement LoadHandler

--- FamilyMapServerStudent-master/src/main/java/handler/LoadHandler.java
+++ FamilyMapServerStudent-master/src/main/java/handler/LoadHandler.java
@@ -3,23 +3,28 @@ package handler;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import service.ClearService;
-import service.results.ClearResult;
+import service.LoadService;
+import service.requests.LoadRequest;
+import service.results.LoadResult;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.net.HttpURLConnection;
 
-public class ClearHandler implements HttpHandler {
+public class LoadHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         try {
             // Confirm the Request Method is a POST Method
             if (exchange.getRequestMethod().toUpperCase().equals("POST")) {
-                // Execute clear logic via ClearService object
-                ClearService service = new ClearService();
-                ClearResult result = service.clear();
+
+                // Deserialize JSON String to Request Object
+                Gson gson = new Gson();
+                Reader reqBody = new InputStreamReader(exchange.getRequestBody());
+                LoadRequest request = (LoadRequest)gson.fromJson(reqBody, LoadRequest.class);
+
+                // Execute Load Logic via LoadService Object
+                LoadService service = new LoadService();
+                LoadResult result = service.load(request);
 
                 // Send HTTP Response
                 if (result.isSuccess()) {
@@ -28,11 +33,11 @@ public class ClearHandler implements HttpHandler {
                     exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
                 }
 
-                // Serialize ClearResult to JSON String
-                Gson gson = new Gson();
+                // Serialize LoadResult to JSON String
                 Writer resBody = new OutputStreamWriter(exchange.getResponseBody());
                 gson.toJson(result, resBody);
                 resBody.close();
+
             } else {
                 // The Request Method was not of type POST
                 exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_METHOD, 0);
