a1aa3cf0e21e7a2dd2c6c32a2b0819002c48aeee
Ben Kinard <benjaminjameskinard@gmail.com>
2021-10-28 21:47:31 -0600

    Implement RegisterHandler

---
+++ FamilyMapServerStudent-master/src/main/java/handler/RegisterHandler.java
@@ -0,0 +1,55 @@
+package handler;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import service.RegisterService;
+import service.requests.RegisterRequest;
+import service.results.RegisterResult;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+
+public class RegisterHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        try {
+            if (exchange.getRequestMethod().toUpperCase().equals("POST")) {
+
+                // Deserialize JSON String to Request Object
+                Gson gson = new Gson();
+                Reader reqBody = new InputStreamReader(exchange.getRequestBody());
+                RegisterRequest request = (RegisterRequest)gson.fromJson(reqBody, RegisterRequest.class);
+
+                // Execute register logic via RegisterService object
+                RegisterService service = new RegisterService();
+                RegisterResult result = service.register(request);
+
+                // Send HTTP Response
+                if (result.isSuccess()) {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                } else {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+                }
+
+                // Serialize RegisterResult to JSON String
+                Writer resBody = new OutputStreamWriter(exchange.getResponseBody());
+                gson.toJson(result, resBody);
+                resBody.close();
+
+            } else {
+                // The Request Method was not of type POST
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_METHOD, 0);
+                // Close the output stream for the response body
+                exchange.getResponseBody().close();
+            }
+        } catch (IOException e) {
+            // Report an internal server error
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
+            // Close the output stream for the response body
+            exchange.getResponseBody().close();
+            // Log the stack trace
+            e.printStackTrace();
+        }
+    }
+}
