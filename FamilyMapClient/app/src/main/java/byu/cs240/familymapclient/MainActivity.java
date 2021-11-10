9582e73964f9d88886dd2c7c8272c77c92cf321e
Ben Kinard <benjaminjameskinard@gmail.com>
2021-11-09 17:46:21 -0700

    Host LoginFragment in MainActivity

--- FamilyMapClient/app/src/main/java/byu/cs240/familymapclient/MainActivity.java
+++ FamilyMapClient/app/src/main/java/byu/cs240/familymapclient/MainActivity.java
@@ -1,6 +1,8 @@
 package byu.cs240.familymapclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment currentFragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (currentFragment == null) {
+            LoginFragment fragment = new LoginFragment();
+            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
